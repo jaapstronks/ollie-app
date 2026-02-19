ab66465e91f7d4ae60f686e6b37ff1c4e606d31c
Jaap Stronks <jaapstronks@bureaubolster.nl>
2026-02-19 10:28:38 +0100

    Initial Commit

---
+++ Ollie-app/Ollie_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ollie_appApp.swift
+//  Ollie-app
+//
+//  Created by Jaap Stronks on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct Ollie_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
